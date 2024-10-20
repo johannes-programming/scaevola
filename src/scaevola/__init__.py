712c2a4353298b708009f5934e2e16cab4470d04
Johannes <johannes-programming@mailfence.com>
2024-10-20 03:16:12 +0200

    Version 1.0.9

--- src/scaevola/__init__.py
+++ src/scaevola/__init__.py
@@ -1,48 +1,64 @@
 class Scaevola:
-    def __ge__(self, other):
+    def __ge__(self, other, /):
+        """Return other<=self."""
         return type(self)(other) <= self
 
-    def __gt__(self, other):
+    def __gt__(self, other, /):
+        """Return other<self."""
         return type(self)(other) < self
 
-    def __radd__(self, other):
+    def __radd__(self, other, /):
+        """Return other+self."""
         return type(self)(other) + self
 
-    def __rand__(self, other):
+    def __rand__(self, other, /):
+        """Return other&self."""
         return type(self)(other) & self
 
-    def __rdivmod__(self, other):
+    def __rdivmod__(self, other, /):
+        """Return divmod(other, self)."""
         return divmod(type(self)(other), self)
 
-    def __rfloordiv__(self, other):
+    def __rfloordiv__(self, other, /):
+        """Return other//self."""
         return type(self)(other) // self
 
-    def __rlshift__(self, other):
+    def __rlshift__(self, other, /):
+        """Return other<<self."""
         return type(self)(other) << self
 
-    def __rmatmul__(self, other):
+    def __rmatmul__(self, other, /):
+        """Return other@self."""
         return type(self)(other) @ self
 
-    def __rmod__(self, other):
+    def __rmod__(self, other, /):
+        """Return other%self."""
         return type(self)(other) % self
 
-    def __rmul__(self, other):
+    def __rmul__(self, other, /):
+        """Return other*self."""
         return type(self)(other) * self
 
-    def __ror__(self, other):
+    def __ror__(self, other, /):
+        """Return other|self."""
         return type(self)(other) | self
 
-    def __rpow__(self, other):
+    def __rpow__(self, other, /):
+        """Return other**self."""
         return type(self)(other) ** self
 
-    def __rrshift__(self, other):
+    def __rrshift__(self, other, /):
+        """Return other>>self."""
         return type(self)(other) >> self
 
-    def __rsub__(self, other):
+    def __rsub__(self, other, /):
+        """Return other-self."""
         return type(self)(other) - self
 
-    def __rtruediv__(self, other):
+    def __rtruediv__(self, other, /):
+        """Return other/self."""
         return type(self)(other) / self
 
-    def __rxor__(self, other):
+    def __rxor__(self, other, /):
+        """Return other^self."""
         return type(self)(other) ^ self
